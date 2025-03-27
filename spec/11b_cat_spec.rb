aa5c7e77c95a63906f91842d929b8182ba17f358
Ijaz Ahmad <109833495+ijaz94@users.noreply.github.com>
2025-03-27 22:30:46 +0500

    11s are about shared and include examples

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:persian) { described_class.new('persian', 'persian cat', 'white') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when testing the talk method' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(persian).to_not be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(persian).to be_hiding
   end
 end
--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:shepherd) { described_class.new('Shepherd', 'German Shepherd', 'brown') }
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'When dog has method name shared with other classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(shepherd).to_not be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(shepherd).to be_sleeping
   end
 end
